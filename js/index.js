9ed93b82a5ad5d1ffc8adbaeb52317ca5623f867
zequnyu9503 <greatyuzequn@outlook.com>
2020-11-06 15:53:11 +0800

    support Markdown

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ function valueFilter(property, object) {
 	switch (property) {
 		case "id" :
 			if (object['tag'] && object['tag'] === 'md') {
-				value = 'md/' + value + '.md';
+				value = 'articles/md/' + value + '.md';
 			} else {
 				value = 'articles/article.html?page=' + value;
 			}
